21e067f836fc6c4e3e1b973b2298ec087497b32b
bufferunderpants <gabriel.peredo@gmail.com>
2011-01-04 19:39:52 -0300

    Most of the port to leiningen for the build process and dependency management. A directory and a file are missing...

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject Temaworks "0.1-SNAPSHOT"
+  :description "A ZK framework framework"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+				 [clojureql "1.0.0-beta2-SNAPSHOT"]
+				 [org.clojars.rayne/clj-time "0.1.1-SNAPSHOT"]
+		 		 [org.zkoss.zk/zk "5.0.5"]
+		 		 [org.zkoss.zk/zul "5.0.5"]]
+  :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.12"]
+					 [lein-eclipse "1.0.0"]
+					 [swank-clojure "1.2.1"]]
+  :repositories {"zk repository" "http://mavensync.zkoss.org/maven2"}
+  :war {:webxml "WebContent/WEB-INF/web.xml"}
+  :java-source-path "src/temaworks/handling")
---
+++ test/Temaworks/test/core.clj
@@ -0,0 +1,6 @@
+(ns Temaworks.test.core
+  (:use [Temaworks.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
