e5aebf0dc3a8577dbfcf2857677df73f89132629
bufferunderpants <gabriel.peredo@gmail.com>
2011-01-04 21:52:22 -0300

    Cambios en los espacios de nombres para acomodar a leiningen

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject Temaworks "0.1-SNAPSHOT"
+(defproject temaworks "0.1-SNAPSHOT"
   :description "A ZK framework framework"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
@@ -11,4 +11,5 @@
 					 [swank-clojure "1.2.1"]]
   :repositories {"zk repository" "http://mavensync.zkoss.org/maven2"}
   :war {:webxml "WebContent/WEB-INF/web.xml"}
-  :java-source-path "src/temaworks/handling")
+  :java-source-path "src/temaworks/handling"
+  :main "temaworks")
